716669e07e8f56cd41509f08a078bb7f1a97115a
Raffaella Tramontano <raffaella.tram@gmail.com>
2023-01-13 10:38:05 +0100

    script for datacards handling

--- python/datacardsHandler.py
+++ python/datacardsHandler.py
@@ -71,7 +71,7 @@ def MoveSysts(datacard,Oldvalue,newValue):
 
 
 #def CouplingScaling(idatacard,f):
-
+#might be better to create 2 directories containg the datacards and accessed through glob.glob
 MuonCards = ["../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_1_v2_5p4em02_cat_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_1_v2_5p4em03_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_1p5_v2_7p1em03_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_1p5_v2_7p1em04_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_1p5_v2_7p1em05_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_2_v2_1p7em03_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_2_v2_1p7em04_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_2_v2_1p7em05_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_3_v2_2p2em03_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_3_v2_2p2em04_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_3_v2_2p2em05_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_3_v2_2p2em06_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_4p5_v2_2p9em03_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_4p5_v2_2p9em04_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_4p5_v2_2p9em05_combined.txt","../output/counting_experiment_realData_lumiD1/datacard_bhnl_m_4p5_v2_2p9em06_combined.txt"]
 ElectronCards = ["../output/PFCards/HNL_M1_ctau1000_PF_OSxSS_combined.txt","../output/PFCards/HNL_M1_ctau100_PF_OSxSS_combined.txt","../output/PFCards/HNL_M1_ctau10_PF_OSxSS_combined.txt","../output/PFCards/HNL_M2_ctau1000_PF_OSxSS_combined.txt","../output/PFCards/HNL_M2_ctau100_PF_OSxSS_combined.txt","../output/PFCards/HNL_M2_ctau10_PF_OSxSS_combined.txt","../output/PFCards/HNL_M3_ctau1000_PF_OSxSS_combined.txt","../output/PFCards/HNL_M3_ctau100_PF_OSxSS_combined.txt","../output/PFCards/HNL_M3_ctau10_PF_OSxSS_combined.txt","../output/PFCards/HNL_M3_ctau1_PF_OSxSS_combined.txt","../output/PFCards/HNL_M4_ctau0_PF_OSxSS_combined.txt","../output/PFCards/HNL_M4_ctau100_PF_OSxSS_combined.txt","../output/PFCards/HNL_M4_ctau10_PF_OSxSS_combined.txt","../output/PFCards/HNL_M4_ctau1_PF_OSxSS_combined.txt"]
 #for card in MuonCards:
@@ -80,3 +80,10 @@ ElectronCards = ["../output/PFCards/HNL_M1_ctau1000_PF_OSxSS_combined.txt","../o
 #	scaleYields("../"+card.strip(".txt")+"ScaledFullLumi.txt",0.5*0.5,True,"_fmu0p5",1)
 for card in ElectronCards:
 	scaleYields(card,0.5*0.5,True,"_fmu0p5",0)
+
+
+if fullCombination:
+
+    
+	
+    print("python ~/CMSSW_10_2_15/src/HiggsAnalysis/CombinedLimit/scripts/combineCards.py "+ combinedString+" >> " +combinedCard)
