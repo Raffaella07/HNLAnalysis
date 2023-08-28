844c931a6fa420754c3b5a9083638922688db89f
Raffaella Tramontano <raffaella.tram@gmail.com>
2023-08-28 16:08:23 +0200

    minors

--- macros/LifetimeReweight.C
+++ macros/LifetimeReweight.C
@@ -12,7 +12,7 @@
 #include "TFile.h"
 #include "TAxis.h"
 
-void sig_fromcsv(std::string filename, std::vector<std::string>* flatIn,std::vector<std::string>* label, std::vector<double>* genEventCount,std::vector<double>* filterEff,std::vector<double>* mass,std::vector<double>* ctau, std::vector<double>* sigma,std::vector<double>* Flav_ratio){
+void sig_fromcsv(std::string filename, std::vector<std::string>* flatIn,std::vector<std::string>* label, std::vector<double>* genEvent_Bu,std::vector<double>* genEvent_Bd,std::vector<double>* genEvent_Bs,std::vector<double>* genEvent,std::vector<double>* filterEff_Bu,std::vector<double>* filterEff_Bd,std::vector<double>* filterEff_Bs,std::vector<double>* filterEff,std::vector<double>* mass,std::vector<double>* ctau, std::vector<double>* sigma,std::vector<double>* Flav_ratio){
 
 	fstream fin;
 
@@ -49,16 +49,22 @@ void sig_fromcsv(std::string filename, std::vector<std::string>* flatIn,std::vec
 			counter++;
 			row.push_back(word);
 		}
-		flatIn->push_back(row[9].c_str());
+		flatIn->push_back(row[15].c_str());
 		label->push_back(row[1]);
 //		std:: cout << flatIn->at(0)<< std::endl;
-		genEventCount->push_back(atof(row[5].c_str()));
-//		std:: cout << genEventCount->at(0)<< std::endl;
-		filterEff->push_back(atof(row[6].c_str()));
+		genEvent_Bu->push_back(atof(row[5].c_str()));
+		genEvent_Bd->push_back(atof(row[6].c_str()));
+		genEvent_Bs->push_back(atof(row[7].c_str()));
+		genEvent->push_back(atof(row[8].c_str()));
+		filterEff_Bu->push_back(atof(row[9].c_str()));
+		filterEff_Bd->push_back(atof(row[10].c_str()));
+		filterEff_Bs->push_back(atof(row[11].c_str()));
+		filterEff->push_back(atof(row[12].c_str()));
+		//std:: cout << row[12]<< std::endl;
 		mass->push_back(atof(row[2].c_str()));
 		ctau->push_back(atof(row[3].c_str()));
-		sigma->push_back(atof(row[7].c_str()));
-		Flav_ratio->push_back(atof(row[8].c_str()));
+		sigma->push_back(atof(row[13].c_str()));
+		Flav_ratio->push_back(atof(row[14].c_str()));
 
 
 		// Compare the roll number
@@ -81,14 +87,14 @@ void sig_fromcsv(std::string filename, std::vector<std::string>* flatIn,std::vec
 
 
 
-double LifetimeReweight(float mass, float ctau,float ct){
+double LifetimeReweight(std::string csvIn,float mass, float ctau,float ct,int flavor){
 
 
-	std::vector<double> GenEvtCount,Filter,Mass,Ctau,Sigma,Flav_ratio;
+	std::vector<double> GenEvent_Bu, GenEvent_Bd, GenEvent_Bs,GenEvtCount,Filter_Bu,Filter_Bd,Filter_Bs,Filter,Mass,Ctau,Sigma,Flav_ratio;
 	std::vector<std::string> Files, label;
-
 	
-	sig_fromcsv("../data/MC_datasets_InclusiveFilterEff.csv",&Files,&label,&GenEvtCount,&Filter,&Mass,&Ctau,&Sigma,&Flav_ratio);
+	//sig_fromcsv("../data/MC_datasets_InclusiveFilterEff.csv",&Files,&label,&GenEvtCount,&Filter,&Mass,&Ctau,&Sigma,&Flav_ratio);
+	sig_fromcsv(csvIn.c_str(),&Files,&label,&GenEvent_Bu,&GenEvent_Bd,&GenEvent_Bs,&GenEvtCount,&Filter_Bu,&Filter_Bd,&Filter_Bs,&Filter,&Mass,&Ctau,&Sigma,&Flav_ratio);
 
 //dummy initialization for miniAOD genevt count numbers 
 /*	double genEvtCount[5][4]={{9.080620000000000000e+05,4.532249000000000000e+06,1.312593500000000000e+07,-1},
@@ -120,35 +126,52 @@ double LifetimeReweight(float mass, float ctau,float ct){
 	long double GenCountTot=0;
 	double avgFilter=0;
 
-
 	int f;
-//	std::cout << "mass " << mass<< std::endl;
 		for(int i=0;i<Files.size();i++){
 
-			if (mass==Mass[i] && Ctau[i]>0){
+//			std::cout << "mass " << mass<< " " << Mass[i]-mass <<  " ctau" << Ctau[i] <<  std::endl;
+			if (fabs(mass-Mass[i])/mass<0.000001 && Ctau[i]>0. ){
+				if (flavor ==4 || flavor ==3){	
+
 				den += GenEvtCount[i]/(Ctau[i]*Filter[i]) * exp(-ct/(Ctau[i]));
-//				if (Ctau[i]<100*ct)	std::cout << "suppression" << Ctau[i] << " "  << ct << " " <<  exp(-ct/Ctau[i])<< std::endl;
-				GenCountTot+=GenEvtCount[i]*Flav_ratio[i];
-				avgFilter+=GenEvtCount[i]*Flav_ratio[i]*Filter[i];
+				GenCountTot+=GenEvtCount[i];
+				avgFilter+=GenEvtCount[i]*Filter[i];
 
-							}
+				}else if (flavor == 0){
 
+				den += GenEvent_Bu[i]/(Ctau[i]*Filter_Bu[i]) * exp(-ct/(Ctau[i]));
+				GenCountTot+=GenEvent_Bu[i];
+				avgFilter+=GenEvent_Bu[i]*Filter_Bu[i];
+		
+				}else if (flavor == 1){
+
+				den += GenEvent_Bd[i]/(Ctau[i]*Filter_Bd[i]) * exp(-ct/(Ctau[i]));
+				GenCountTot+=GenEvent_Bd[i];
+				avgFilter+=GenEvent_Bd[i]*Filter_Bd[i];
+
+				}else if (flavor == 2){
+
+				den += GenEvent_Bs[i]/(Ctau[i]*Filter_Bs[i]) * exp(-ct/(Ctau[i]));
+				GenCountTot+=GenEvent_Bs[i];
+				avgFilter+=GenEvent_Bs[i]*Filter_Bs[i];
 
 				}
 	
 
-	  		
+	  		}
+	}
 		
 	avgFilter = avgFilter/GenCountTot;
-/*
-  	std::cout << "ctau " << ctau<< std::endl;
+	long double num = GenCountTot*1.0/(ctau*avgFilter) * exp(-ct/(ctau));
+/*	if (num !=0){
+  	std::cout << "ctau " << ctau<<" ct " << ct  << std::endl;
   	std::cout << "avgFilter" << avgFilter<< std::endl;
   	std::cout << "GenEvtCount" << GenCountTot << std::endl;
-  	std::cout << "suppression" << exp(-ct/(ctau))<< std::endl;*/
-	long double num = GenCountTot*1.0/(ctau*avgFilter) * exp(-ct/(ctau));
-/*  	std::cout << "num " << num << std::endl;
-  	std::cout << "den " << den << std::endl;
-  	std::cout << "weight " << num*1.0/den << std::endl;*/
+  	std::cout << "suppression" << TMath::Exp(-ct/(ctau))<< std::endl;
+	}*/
+/*	std::cout << "num " << num << std::endl;
+ 	std::cout << "den " << den << std::endl;*/
+//  	std::cout << "weight " << num*1.0/den << std::endl;
 	return num/den;	
 
 }
